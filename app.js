798882a5ed69735e88b5cd70c17501aac5ebdf0b
ludralph <raphaelumoh@gmail.com>
2022-06-24 15:57:09 +0100

    add endpoint for fetching all parcels

--- app.js
+++ app.js
@@ -7,13 +7,35 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors())
 
-app.get('/', (req, res) => {
+const parcels = [
+    {
+       Id: 1,
+       product: "Phone",
+       description: "An Apple iPhone",
+       deliveryDate: "05/06/2022",
+   },
+    {
+       Id: 2,
+       product: "Laptop",
+       description: "A MacBook Air",
+       deliveryDate: "07/06/2022",
+   },
+    {
+       Id: 3,
+       product: "Bag",
+       description: "A Birkin Bag",
+       deliveryDate: "08/06/2022",
+   }
+   
+   ]
+
+app.get('/parcels', (req, res) => {
     res.json({
-        message: "Hello World"
+        data: parcels 
     })
 })
 
 
 app.listen(PORT, () => {
     console.log(`connected to PORT ${PORT}`);
-})
+});
